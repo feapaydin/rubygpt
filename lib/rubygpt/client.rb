cb55e713042d98d6f7dede19df3608e342883fc0
Furkan Enes <feapaydin@gmail.com>
2024-02-20 23:09:26 +0300

    allow tapping the config, validate on client

--- lib/rubygpt/client.rb
+++ lib/rubygpt/client.rb
@@ -3,13 +3,17 @@
 module Rubygpt
   # Main class that issues the connection to OpenAI APIs
   class Client
+    # The configuration object for the client
+    attr_accessor :configuration
+
     # Initializes new Rubygpt::Client object
     #
     # @param [Hash] configuration
     # @param [Configuration] configuration
-    def initialize(configuration)
+    def initialize(configuration = nil)
       @configuration = Configuration.from(configuration)
       yield @configuration if block_given?
+      @configuration.validate!
     end
   end
 end
